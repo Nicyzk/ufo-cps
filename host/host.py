f4eb025db41c170ab0f5bbdaca68bf724cf1f60d
Nicholas <nicyzk@gmail.com>
2024-12-26 21:14:18 +0000

    dynamic load simulation for rorke

--- host/host.py
+++ host/host.py
@@ -34,25 +34,64 @@ def get_reader_cv_data(cid, req_key):
             if req_key in reader_cv_data[cid]:
                 resp = copy.deepcopy(reader_cv_data[cid])
                 reader_cv_data[cid] = {}
-    print(f"debugging: with req_key {req_key}, see data {resp}")
+    # print(f"debugging: with req_key {req_key}, see data {resp}")
     return resp
+
+def reset_vcpu_pins(config, vm_cid):
+    print(f"vm {vm_cid}, resetting all vcpu pins")
+    vm_config = utils.get_vm_config_by_cid(config, vm_cid)
+    vm_name = vm_config["vm_name"]
+    
+    # get count of vcpus of vm
+    cmd = f"sudo virsh vcpucount {vm_name}"
+    output = utils.run_command(cmd)
+    vcpu_count = next(int(word) for word in output.split() if word.isdigit())
+
+    # turn on every vcpu
+    msg = { "vcpu_cnt_request": vcpu_count }
+    conns[vm_cid].sendall(json.dumps(msg).encode())
+    
+    # guest vm returns adjusted vcpu_id
+    resp = get_reader_cv_data(vm_cid, "vcpu_ids")
+    vcpu_ids = resp["vcpu_ids"]
+   
+    # reset all vcpus
+    for i in range(vcpu_count):
+        cmd = f"sudo virsh vcpupin {vm_name} {i} r"
+        print(f"Running: {cmd}")
+        utils.run_command(cmd)
+    
+    # create runtime_config for vm
+    with runtime_vm_configs_lock:
+        runtime_config = runtime_vm_configs[vm_cid] = {}
+
 
 # initializes a guest vm
 def init_guest(conn, cid):
-    # create log file handler for vm
     global log_fds
+    global conns
+    global config
+    
+    # create log file handler for vm
     log_fd = open(f"./logs/log_{cid}", "a")
     log_fds[cid] = log_fd
 
     # save conn in a global variable
-    global conns
     conns[cid] = conn
 
     # set up condition variables for reading
     reader_cv_data[cid] = {}
     reader_cv[cid] = threading.Condition()
 
-# this adjusts the core assignment mapping, but does not actually change core allocation. It occurs at start of simulation and periodically thereafter
+    # set up reading thread 
+    t = threading.Thread(target=client_reader, args=(cid,), daemon=True)
+    t.start()
+
+    # reset all vcpu pins
+    reset_vcpu_pins(config, cid)
+
+
+# Only for UFO! This adjusts the core assignment mapping, but does not actually change core allocation. It occurs at start of simulation and periodically thereafter
 def adjust_pcpu_to_vm_mapping():
     global config
     global runtime_vm_configs
@@ -65,13 +104,11 @@ def adjust_pcpu_to_vm_mapping():
     if total_vms == 0 or total_cpus == 0:
         print("No VMs or CPUs available for assignment")
     
-    # if the simulation has not started, we should assign pcpus to each vm according to the config file
+    # if the simulation has not started, we simply assign pcpus fairly to each vm
     if not sim_started:
-        total_cpus_req = sum(vm["pcpu"] for vm in config)
-
         cpu_idx = 0
         for vm in config:
-            cpus_req = math.floor((vm["pcpu"] / total_cpus_req) * total_cpus)
+            cpus_req = total_cpus // total_vms
             with runtime_vm_configs_lock:
                 runtime_config = runtime_vm_configs.setdefault(vm["vm_cid"], {})
                 runtime_config["cpus"] = cpu_list[cpu_idx: cpu_idx+cpus_req]
@@ -88,10 +125,15 @@ def adjust_pcpu_to_vm_mapping():
             cnt_cpus_req = {}
             for (cid, runtime_config) in runtime_vm_configs.items():
                 cnt_cpus_req[cid] = math.floor((runtime_config["threads"] / total_threads) * total_cpus)
-                    
-            spare_cpus = []
+            
+            # spare cpus initialized to cpus that have not been pinned previously
+            spare_cpus = utils.get_cpu_list()
+            for runtime_config in runtime_vm_configs.values():
+                for cpu in runtime_config["cpus"]:
+                    spare_cpus.remove(cpu)
+            
            
-           # collect spare cpus
+           # add to spare cpus if cpu is no longer required
             for (cid, runtime_config) in runtime_vm_configs.items():
                 while len(runtime_config["cpus"]) > cnt_cpus_req[cid]:
                     spare_cpus.append(runtime_config["cpus"].pop())
@@ -102,7 +144,7 @@ def adjust_pcpu_to_vm_mapping():
                     runtime_config["cpus"].append(spare_cpus.pop())
 
 
-# adjust number of vcpus to match pcpu and then apply cpu pinning. Note UFO's assumption is that 1 vcpu maps to 1 cpu.
+# Only for UFO! Adjust number of vcpus to match pcpu and then apply cpu pinning. Note UFO's assumption is that 1 vcpu maps to 1 cpu.
 def apply_vcpu_pinning():
     global conns
     global sim_started
@@ -131,9 +173,6 @@ def apply_vcpu_pinning():
 
     # if the simulation has already started, we need to 1. adjust vcpu count 2. vcpu_ids are misaligned with vcpu_cpu_mapping, adjust mapping 3. pin newly-added vcpus on spare cpus
     elif sim_started:
-        spare_cpus = []
-        vm_vcpu_ids_adjusted = {}
-
         with runtime_vm_configs_lock:
             # adjust vcpu count of each vm to match cpu count
             for cid, runtime_config in runtime_vm_configs.items():
@@ -144,31 +183,46 @@ def apply_vcpu_pinning():
                 # guest vm returns adjusted vcpu_id
                 resp = get_reader_cv_data(cid, "vcpu_ids")
                 vcpu_ids = resp["vcpu_ids"]
-                vm_vcpu_ids_adjusted[cid] = vcpu_ids
 
-                # if vcpus are in the old vcpu_cpu_mapping of the vm, but not in vcpu_ids, it means these ids have been removed. We add their cpus to spare_cpus and update the mapping.
+                # variable initializations
                 vm_vcpu_cpu_mapping = runtime_config["vcpu_cpu_mapping"]
                 vm_vcpu_cpu_mapping_new = copy.deepcopy(vm_vcpu_cpu_mapping)
+                allocated_cpu_ids = copy.deepcopy(runtime_config["cpus"])
+                vcpu_ids_that_req_pinning = []
+                
+                # for vcpus that have been unplugged, remove them from mapping. the vcpus in new map is now a subset of vcpu_ids
                 for vcpu_id in vm_vcpu_cpu_mapping.keys():
                     if vcpu_id not in vcpu_ids:
-                        spare_cpus.append(vm_vcpu_cpu_mapping[vcpu_id])
                         del vm_vcpu_cpu_mapping_new[vcpu_id]
+                
+                # identify newly added vcpus and cpus that are available to pin
+                for vcpu_id in vcpu_ids:
+                    if vcpu_id in vm_vcpu_cpu_mapping_new:
+                        allocated_cpu_ids.remove(vm_vcpu_cpu_mapping_new[vcpu_id])
+                    else:
+                        vcpu_ids_that_req_pinning.append(vcpu_id)
+                
+                # match the newly added vcpus to available cpus
+                for i, vcpu_id in enumerate(vcpu_ids_that_req_pinning):
+                    vm_vcpu_cpu_mapping_new[vcpu_id] = allocated_cpu_ids[i]
 
                 runtime_config["vcpu_cpu_mapping"] = vm_vcpu_cpu_mapping_new
 
-            # redistribute the spare cpus to newly-added vcpu_ids
-            for cid, runtime_config in runtime_vm_configs.items():
-                # if ids are in vcpu_ids, but not in vcpu_cpu_mapping of the vm, these ids are newly added and do not have cpus assigned yet. We pin these vcpus to the spare cpus.  
-                vm_vcpu_cpu_mapping = runtime_config["vcpu_cpu_mapping"]
-                vm_vcpu_cpu_mapping_new = copy.deepcopy(vm_vcpu_cpu_mapping)
-                for vcpu_id in vm_vcpu_ids_adjusted[cid]:
-                    if vcpu_id not in vm_vcpu_cpu_mapping.keys():
-                        cpu = spare_cpus.pop()
-                        vm_vcpu_cpu_mapping_new[vcpu_id] = cpu
-                        pin_vcpu_on_cpu(cid, vcpu_id, cpu)
-                runtime_config["vcpu_cpu_mapping"] = vm_vcpu_cpu_mapping_new
+                # verification
+                if len(vcpu_ids_that_req_pinning) != len(allocated_cpu_ids):
+                    print(f"Logical error to fix: unable to match cpus to vcpus!")
+                
+                mapping = runtime_config["vcpu_cpu_mapping"]
+                if len(mapping) != len(vcpu_ids) or len(mapping) != len(runtime_config["cpus"]):
+                    print(f"Logical error to fix: mismatch is size between mapping and vcpu/cpu count!")
+                
+                for vcpu_id, cpu_id in mapping.items():
+                    if vcpu_id not in vcpu_ids or cpu_id not in runtime_config["cpus"]:
+                        print(f"Logical error to fix: vcpu or cpu found in mapping, but not allocated!")
+                        break
 
 
+# Only for UFO!
 def pin_vcpu_on_cpu(vm_cid, vcpu_id, pcpu_id):
     global config
     vm_config = utils.get_vm_config_by_cid(config, vm_cid)
@@ -178,9 +232,8 @@ def pin_vcpu_on_cpu(vm_cid, vcpu_id, pcpu_id):
     utils.run_command(cmd)
 
 
-# a callback that runs every 5 seconds to redistribute cores to vms
+# Only for UFO! A callback that runs every 5 seconds to redistribute cores to vms
 def core_allocation_callback():
-    print(f"running in core allocation...")
     while True:
         adjust_pcpu_to_vm_mapping()
         apply_vcpu_pinning()
@@ -196,7 +249,7 @@ def adjust_workload(max_threads, percentage_load, interval, cid):
     # run workload on guest vm
     new_workload = int(max_threads*percentage_load)
     msg = { "threads": new_workload, "interval": interval }
-    print(f"sent msg to guest {msg}")
+    print(f"sent to vm with cid: {cid}, msg: {msg}")
     conns[cid].sendall(json.dumps(msg).encode())
 
     # track the new workload on vm
@@ -205,7 +258,7 @@ def adjust_workload(max_threads, percentage_load, interval, cid):
 
     # guest vm replies when workload is completed
     resp = get_reader_cv_data(cid, "workload_completed")
-    print(f"guest vm replies {resp}, continuing to next workload")
+    print(f"vm with cid:{cid} completed workload with response:{resp}, continuing to next workload")
 
 
 def sim_workload(max_threads, slices, cid):
@@ -242,6 +295,7 @@ if __name__ == "__main__":
     cli_parser = subparsers.add_parser("cli", help="cli help")
     sim_parser = subparsers.add_parser("sim", help="sim help")
     sim_parser.add_argument("config_file")
+    sim_parser.add_argument("sched", choices=["ufo", "rorke"])
     args = parser.parse_args()
     
     # start vsock server
@@ -259,7 +313,8 @@ if __name__ == "__main__":
     elif args.mode == "sim":
         config_fd = open(args.config_file, "r")
         config = json.loads(config_fd.read())
-        
+       
+
         # each client has a simulator thread
         client_sim_threads = []
 
@@ -276,32 +331,30 @@ if __name__ == "__main__":
                 sys.exit()
             else:
                 expected_vms.remove(remote_cid)
-            
+           
             init_guest(conn, remote_cid)
             
             t = threading.Thread(target=run_sim, args=(remote_cid,))
-            client_sim_threads.append(t)
-            
-            t = threading.Thread(target=client_reader, args=(remote_cid,))
-            client_reader_threads.append(t)
+            client_sim_threads.append(t)     
             
             print(f"guest {remote_cid} has connected")
             if len(expected_vms) == 0:
                 break
        
-        for t in client_reader_threads:
-            t.start()
-        
-        adjust_pcpu_to_vm_mapping()
-        apply_vcpu_pinning()
-        print(f"runtime_vm_configs (before simulation starts): {runtime_vm_configs}")
-        sim_started = True
+        if args.sched == "ufo":
+            adjust_pcpu_to_vm_mapping()
+            apply_vcpu_pinning()
+            print(f"runtime_vm_configs (before simulation starts): {runtime_vm_configs}")
+            sim_started = True
         
         for t in client_sim_threads:
             t.start()
+        
+        if args.sched == "ufo":
+            t = threading.Thread(target=core_allocation_callback, daemon=True)
+            t.start()
+        
+        for t in client_sim_threads:
+            t.join()
 
-        t = threading.Thread(target=core_allocation_callback)
-        t.start()
-
-        while True:
-            pass
+        print("All client simulations completed. Program exiting.") 
