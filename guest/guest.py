72414c6bc002b141001a73b8f8bee23eed16a082
Anirudh Varanasi <av3162@columbia.edu>
2024-12-29 09:05:42 +0000

    bug fix in guest.py for multiple workloads

--- guest/guest.py
+++ guest/guest.py
@@ -20,16 +20,17 @@ def run_ufo(s, log_file):
         if "vcpu_cnt_request" in data:
             resize_cpus_thread = threading.Thread(target=ufo.resize_cpus_ufo, args=(s, data,))
             resize_cpus_thread.start()
-        elif "mutex" in data:
-            sysbench_thread = threading.Thread(target=utils.run_sysbench, args=(s, data, log_file, True))
-            sysbench_thread.start()
-        elif "sysbench" in data:
-            print(f"running sysbench with data {data}")
-            sysbench_thread = threading.Thread(target=utils.run_sysbench, args=(s, data, log_file, False))
-            sysbench_thread.start()
-        elif "redis" in data:
-            redis_thread = threading.Thread(target=utils.run_redis, args=(s, data, log_file))
-            redis_thread.start()
+        elif "threads" in data:
+            if "mutex" == data["workload"]:
+                sysbench_thread = threading.Thread(target=utils.run_sysbench, args=(s, data, log_file, True))
+                sysbench_thread.start()
+            elif "sysbench" == data["workload"]:
+                print(f"running sysbench with data {data}")
+                sysbench_thread = threading.Thread(target=utils.run_sysbench, args=(s, data, log_file, False))
+                sysbench_thread.start()
+            elif "redis" == data["workload"]:
+                redis_thread = threading.Thread(target=utils.run_redis, args=(s, data, log_file))
+                redis_thread.start()
         
 
 def run_cps(s):  
